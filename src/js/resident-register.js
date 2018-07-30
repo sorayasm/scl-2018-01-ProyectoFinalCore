de513081fdc01263d4ce58614030eafe6541663e
SorayaSM <soraya.sacaan@gmail.com>
2018-07-29 22:43:46 -0400

    formulario residentes

--- src/js/resident-register.js
+++ src/js/resident-register.js
@@ -1,11 +1,14 @@
 const rname = residentName.value;
-const rdni = residentDNI.valu;
+const rdni = residentDNI.value;
 const rposition = residentPosition.value;
 const rcompany = residentCompany.value;
 const rmail = residentMail.value;
-const rphone = residentPhone.value;
+//const rphone = residentPhone.value;
+
+firebase.initializeApp(config);
 
 function newResident() {
+
     const rootRef = firebase.database().ref();
     const storesRef = rootRef.child("residentes");
     const newStoreRef = storesRef.push();
@@ -15,7 +18,7 @@ function newResident() {
         company: rcompany,
         position: rposition,
         mail: rmail,
-        photo: rphoto
+        //photo: rphoto
     });
 }
 
