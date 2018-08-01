c4df8475afdc3722c80adc78e1305bc1ad3bc43f
Dennisse Moreno C <deni.moreno11@gmail.com>
2018-07-31 20:02:54 -0400

    Lista comunidad

--- src/js/community-list.js
+++ src/js/community-list.js
@@ -17,7 +17,55 @@ function gotData(data) {
         //console.log(names, dni, enterTime, motive, patente, time, mail)
         const appendingTo = document.getElementById("residentlist");
         const div = document.createElement("div")
-        div.innerHTML = names + " " + dni + " " + company + " " + mail + " " + phone + " " + position;
+        //div.innerHTML = names + " " + dni + " " + company + " " + mail + " " + phone + " " + position;
+        div.innerHTML = 
+        `<div id="publicar">
+            <div class="row">
+                <div class="col-4 col-md-4 col-lg-4">
+                    <div class="nombre">
+                        <h5>Nombre Completo</h5>
+                        <h5 class="font-weight-bold">${names}</h5>
+                    </div>
+                </div>
+                <div class="col-4 col-md-4 col-lg-4">
+                    <div class="correo">
+                        <h5>Correo</h5>
+                        <h5 class="font-weight-bold">${mail}</h5>
+                    </div>
+                </div>
+                <div class="col-4 col-md-4 col-lg-4">
+                    <div class="cargo">
+                        <h5>Cargo</h5>
+                        <h5 class="font-weight-bold">${position}</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-4 col-md-4 col-lg-4">
+                    <div class="rut">
+                        <h5>Rut/DNI/Pasaporte</h5>
+                        <h5 class="font-weight-bold">${dni}</h5>
+                    </div>
+                </div>
+                <div class="col-4 col-md-4 col-lg-4">
+                    <div class="empresa">
+                        <h5>Empresa</h5>
+                        <h5 class="font-weight-bold">${company}</h5>
+                    </div>
+                </div>
+                <div class="col-4 col-md-4 col-lg-4">
+                    <div class="teléfono">
+                        <h5>Teléfono</h5>
+                        <h5 class="font-weight-bold">${phone}</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        `
+        
+        
         appendingTo.appendChild(div);
     }
 
